0f5b83b0d9365d58357ade86fb0bb9b6b8cd526e
Riley Paul <rileypaul96@gmail.com>
2023-05-08 10:23:47 -0700

    add comments to sweep import

--- import_macro/import_sweep.py
+++ import_macro/import_sweep.py
@@ -14,20 +14,35 @@ def import_sweep(file: pd.ExcelFile):
       0: Progress has not been claimed
       1: Progress has been claimed
     """
+
+    # parse file using pandas
+    # pandas has excellent docs
+    # refer to the below for all possible arguments
+    # https://pandas.pydata.org/docs/reference/api/pandas.read_excel.html
     data = file.parse(
         sheet_name="2002-LineSweep",
-        usecols="G:H,U:V",
-        header=35,
+        usecols="G:H,U:V",  # only pull in necessary rows. Not best practice to hard code (oh well)
+        header=35,  # define header row. Ignore rows above this
     )
 
+    # data is of the form KP_beg,KP_end,2-way,4-way
+    # where cells in the 2-way or 4-way columns having values are considered complete
+    # cells that are blank are considered incomplete
+    # there are some rows with the letter x in them that need to be cleaned
+    # the columns will also be renamed
+    print("original sweep")
+    print(data)
+
+    # rename columns
     columns = ["KP_beg", "KP_end", 2002.1, 2002.2]
-    data = (
-        data.rename({i: j for i, j in zip(data.columns, columns)}, axis=1)
-        .set_index(["KP_beg", "KP_end"])
-        .drop("x")
-        .notnull()
-        .astype("int")
-    )
+    data.columns = columns
+
+    # set the index of the dataframe to the KP columns
+    # this removes these columns from the dataset so they aren't effected by global operations
+    # drop all rows containing "x"
+    # convert the dataset to true false using notnull based on whether a cell is blank
+    # convert the type of the dataset to int so it is just 1 and 0
+    data = data.set_index(["KP_beg", "KP_end"]).drop("x").notnull().astype("int")
 
     print("Macro sweep imported")
     return data
@@ -39,7 +54,7 @@ def import_sweep(file: pd.ExcelFile):
 if __name__ == "__main__":
     fname = "raw/Master Progress Report_2023 05 05_MacroNK - Copy.xlsx"
     file = pd.ExcelFile(fname)
-    
+
     result = import_sweep(file)
     result.to_csv("test.csv")
     print(result)
